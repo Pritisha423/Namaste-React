1bdce646d44b0e3396548f735d9b2f387f517703
Pritisha423 <pritisha191999@gmail.com>
2024-10-06 13:02:19 +0530

    Complete Inception

---
+++ App.js
@@ -0,0 +1,14 @@
+const heading = React.createElement(
+    "div",
+    { id: "parent" },
+    React.createElement(
+        "div",
+        { id: "child" },
+        React.createElement("h1", {}, "Hello World from H1 Tag")
+    )
+);
+
+console.log(heading);  // here heading is an object it will give react element which is in actual a javascript object.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
