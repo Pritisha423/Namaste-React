8dc851f5dd406efc7de4785db044c5d9b953bc98
Pritisha423 <pritisha191999@gmail.com>
2024-10-16 01:06:51 +0530

    How to code in JSX.

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => (Object) => HTML(rendered) 
-const heading = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement(
-        "div",
-        { id: "child" },
-        React.createElement("h1", {}, "Hello World from H1 Tag")
-    )
+// JSX (transpiled before it reaches the Javascript)
+
+// JSX ==> Bable transpiles it tio React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+const jsxHeading = ( 
+    <h1 className = "head" tabIndex="5"> 
+        Namaste React with JSX🚀
+    </h1>
 );
 
-console.log(heading);  // here heading is an object it will give react element which is in actual a javascript object.
+console.log(jsxHeading);  // here heading is an object it will give react element which is in actual a javascript object.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(jsxHeading);
